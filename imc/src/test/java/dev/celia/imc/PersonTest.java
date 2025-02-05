a600db8f6924ef32db59c1a1b6a356804a2a893e
Celia Garrido <celiagarridoherrera@gmail.com>
2025-02-05 20:14:41 +0100

    PersonTest do not pass

--- imc/src/test/java/dev/celia/imc/PersonTest.java
+++ imc/src/test/java/dev/celia/imc/PersonTest.java
@@ -5,5 +5,12 @@ import java.lang.annotation.Target;
 import org.apereo.services.persondir.support.xml.om.Person;
 
 public class PersonTest {
+    @Test public void testIMC() {
+
+        Person person = new Person(70.00, 1.75);
+        double expectedIMC = 70.00 / Math.pow(1.75, 2);
+        
+        assertEquals(expectedIMC, person.calculateIMC, 0.01);
+    }
 
 }
