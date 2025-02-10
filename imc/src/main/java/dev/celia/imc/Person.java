b6fecd39b30e3cdb39e5b9e186571734e223825a
Celia Garrido <celiagarridoherrera@gmail.com>
2025-02-10 21:21:30 +0100

    implement constructors and getters on class Person

--- imc/src/main/java/dev/celia/imc/Person.java
+++ imc/src/main/java/dev/celia/imc/Person.java
@@ -1,4 +1,18 @@
 package dev.celia.imc;
 
 public class Person {
+    private double weight;
+    private double height;
+    public Person(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public double getHeight() {
+        return height;
+    }
+
+    
     }
